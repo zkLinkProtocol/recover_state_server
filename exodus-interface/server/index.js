f42181fa60174c3b1ccacd2f677362adfad6554e
tyral <tyral@zklink.org>
2023-04-26 22:09:23 +0800

    refactor: remove server logs

--- exodus-interface/server/index.js
+++ exodus-interface/server/index.js
@@ -12,15 +12,6 @@ app.use(
     target: 'http://127.0.0.1:8080',
     changeOrigin: true,
     rewrite: (path) => path.replace(/\/api/, ''),
-    logs: (ctx, target) => {
-      console.log(
-        '%s - %s %s proxy to -> %s',
-        new Date().toISOString(),
-        ctx.req.method,
-        ctx.req.oldPath,
-        new URL(ctx.req.url, target)
-      )
-    },
   })
 )
 
