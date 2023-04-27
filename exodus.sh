3170a8608305f0fa7a739ca570e87520330a1114
RequiemOfSouls <RequiemOfSouls@zklink.org>
2023-04-27 21:49:53 +0800

    update exodus.sh and verification_exit.key

--- exodus.sh
+++ exodus.sh
@@ -3,7 +3,6 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
 
 cd $DIR
-cargo build --release
 
 if [ ! -d "log" ]; then
   mkdir log
@@ -18,6 +17,7 @@ if [ "$1" == "start" ]; then
   cd storage
   diesel database reset
   cd ..
+  cargo build --release
   nohup ./target/release/recover_state --genesis >> log/recover_state.log 2>&1 &
   echo "start recovering state"
   nohup ./target/release/exodus_server >> log/server.log 2>&1 &
@@ -29,21 +29,24 @@ if [ "$1" == "start" ]; then
   npm run build:devnet
   npm run serve:devnet
 elif [ "$1" == "continue" ]; then
+  cargo build --release
   # If there is an interruption, you can run the `continue` command
   nohup ./target/release/recover_state --continue >> log/recover_state.log 2>&1 &
   echo "Continue recovering state"
   nohup ./target/release/exodus_server >> log/server.log 2>&1 &
   echo "Continue exodus server"
-  nohup ./target/release/exodus_prover tasks -w 4 >> log/prover.log 2>&1 &
+  nohup ./target/release/exodus_prover tasks -w 2 >> log/prover.log 2>&1 &
   echo "Continue exodus prover"
   cd exodus-interface
   npm run build:devnet
   npm run serve:devnet
 elif [ "$1" == "server" ]; then
+  cargo build --release
   nohup ./target/release/exodus_server >> log/server.log 2>&1 &
 elif [ "$1" == "prover" ]; then
+  cargo build --release
   # Please refer to prover [README.md](prover/README.md) for detailed command details
-  nohup ./target/release/exodus_prover tasks -w 4 >> log/prover.log 2>&1 &
+  nohup ./target/release/exodus_prover tasks -w 2 >> log/prover.log 2>&1 &
 elif [ "$1" == "stop" ]; then
   pkill -f recover_state
   pkill -f exodus_server
