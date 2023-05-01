b58053c76051a501bca5511c180349a536e54c9e
tyral <tyral@zklink.org>
2023-05-01 15:51:50 +0800

    config: delay

--- exodus-interface/src/config/index.ts
+++ exodus-interface/src/config/index.ts
@@ -1,6 +1,6 @@
 export const STATIC_HOST = process.env.REACT_APP_STATIC_HOST
 export const NODE_NAME = process.env.REACT_APP_NODE_NAME
 
-export const RUNNING_TASK_ID_DELAY = 10000
-export const RECOVER_PROGRESS_DELAY = 10000
+export const RUNNING_TASK_ID_DELAY = 3000
+export const RECOVER_PROGRESS_DELAY = 3000
 export const PENDING_BALANCE_DELAY = 10000
