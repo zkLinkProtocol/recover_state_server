64d9928cbe661808cb6c1911cc2fd5d7a879c2b7
tyral <tyral@zklink.org>
2023-05-09 20:02:48 +0800

    refactor: polling interval

--- exodus-interface/src/config/index.ts
+++ exodus-interface/src/config/index.ts
@@ -1,8 +1,8 @@
 export const STATIC_HOST = process.env.REACT_APP_STATIC_HOST
 export const NODE_NAME = process.env.REACT_APP_NODE_NAME
 
-export const RUNNING_TASK_ID_DELAY = 3000
-export const RECOVER_PROGRESS_DELAY = 3000
-export const PENDING_BALANCE_DELAY = 10000
+export const RUNNING_TASK_ID_DELAY = 30000
+export const RECOVER_PROGRESS_DELAY = 30000
+export const PENDING_BALANCE_DELAY = 30000
 
 export const TESTER = !!window.localStorage.getItem('test_verify')
